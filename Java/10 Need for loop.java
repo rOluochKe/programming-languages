b037fa092635ed196515de5436328f4e6d63f67d
Raymond Oluoch <rasholuoch@gmail.com>
2024-09-12 17:11:16 +0300

    re-learning java

---
+++ Java/10 Need for loop.java
@@ -0,0 +1,27 @@
+public class hello {
+
+  public static void main(String[] args) {
+    // repeat this statement 4 times
+    // loop -while, do while, for
+
+    // 100 - condition
+
+    System.out.println("Hi");
+
+  }
+}
+
+/**
+ * For loop:-
+ * If you know how many iterations you want to go for loop
+ * (intiliazing vaue, condition, increment or decrement)
+ *
+ * While loop:-
+ * When you need to read the file go for while loop, if the number of iterations
+ * is not known.
+ *
+ * - For loop can also be used as a while.
+ *
+ * Do While Loop:-
+ * If you condition get false but you want to exceute the code at least once.
+ */
---
+++ Java/12 Do While Loop.java
@@ -0,0 +1,10 @@
+public class hello {
+
+  public static void main(String[] args) {
+    int i = 1;
+    do {
+      System.out.println("Hi" + i);
+      i++;
+    } while (i <= 4);
+  }
+}
